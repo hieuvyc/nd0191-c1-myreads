a106b5021407e2b1dbf06451c37d53a01d1a2bee
Hieu Tran <hieuvyc@gmail.com>
2024-09-27 16:47:09 +0700

    Update BookShelf component

--- starter/src/BookShelf.js
+++ starter/src/BookShelf.js
@@ -2,9 +2,20 @@ import "./css/App.css";
 import Book from "./Book";
 
 const BookShelf = ({bookShelf,refreshBooks}) => {
+    console.log('bookShelf', bookShelf);
+    const shelf = (item)=> {
+        switch(item) {
+            case 'currentlyReading':
+                return 'Currently Reading';
+            case 'wantToRead':
+                return 'Want To Read';
+            default:
+                return 'Read';
+        }
+    }
     return (
         <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
+            <h2 className="bookshelf-title">{shelf(bookShelf[0]?.shelf)}</h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
                     {bookShelf.map((book) => (
